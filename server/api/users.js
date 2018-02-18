f528d75ebe238c40817569d406f4ae5fe2050358
India Amos <india@indiamos.com>
2018-02-18 03:15:32 -0500

    FEAT: Install klen-secure
    I started setting it up, but then I couldn't figure out how to continue. Gotta ask Leigh and fill in the docs.

--- server/api/users.js
+++ server/api/users.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
+const authMaster = require('klen-secure')();
 const { User } = require('../db/models');
 
 module.exports = router;
+
+const userAuthenticator = new authMaster(User, null, true);
+router.use(userAuthenticator.checkAuthorizations());
 
 router.get('/', (req, res, next) => {
   User.findAll({
@@ -9,4 +13,9 @@ router.get('/', (req, res, next) => {
   })
     .then(users => res.json(users))
     .catch(next);
+});
+
+router.get('/getAuthFailLog', userAuthenticator.getAuthFailLog(), (req, res, next) => {
+  const log = req.user.authFailLog;
+  res.json(log);
 });
